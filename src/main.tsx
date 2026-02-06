a67196cb6464712b4cbb5c048ee109f719a18e0c
ADEEB-only <redwoan.ahmod.adeeb.1@gmail.com>
2026-02-06 18:38:10 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+// Make Telegram callback global
+(window as any).onTelegramAuth = (user: any) => {
+  const tgUser = {
+    telegram_id: user.id,
+    telegram_username: user.username,
+    telegram_name: user.first_name,
+    photo_url: user.photo_url,
+  };
+
+  localStorage.setItem("tg_user", JSON.stringify(tgUser));
+  window.location.reload();
+};
 
 createRoot(document.getElementById("root")!).render(<App />);
